61fa74a1bcca715761ec8417729fc8c8bdfc78c1
c_fuxingyue-001 <5tgb^YHN@qq.com>
2019-11-16 16:03:27 +0800

    feat(add) build

---
+++ precache-manifest.63d7d850d577cfb39c9031b85c63c6f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dde127a683678595850c41fbe48627f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e17878ba976e4f16521",
+    "url": "/static/css/2.d2e72ad4.chunk.css"
+  },
+  {
+    "revision": "38f98f7cca2a6f44007e",
+    "url": "/static/css/main.e50d6d13.chunk.css"
+  },
+  {
+    "revision": "7e17878ba976e4f16521",
+    "url": "/static/js/2.8c14bee6.chunk.js"
+  },
+  {
+    "revision": "38f98f7cca2a6f44007e",
+    "url": "/static/js/main.e0791d2b.chunk.js"
+  },
+  {
+    "revision": "fdac113b0cef8ff93618",
+    "url": "/static/js/runtime-main.31e2422c.js"
+  }
+]);
